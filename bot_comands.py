e60e61c821c9fa3c1a86b534dbdc1784586de3fa
Sergei <energostuk@gmail.com>
2022-08-16 12:17:44 +0300

    Add all fuctions

--- bot_comands.py
+++ bot_comands.py
@@ -4,7 +4,6 @@ from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, Conve
 from config import TOKEN
 
 
-
 def first_turn_tossup() -> bool:
     toss = False
     if (random.randint(1, 9) % 2 == 0):
@@ -18,71 +17,44 @@ def basic_bot_player() -> int:
 
 
 def mind_bot_player(player_turn: int) -> int:
-    bot_turn = 29 - player_turn
+    global candies_turn
+    bot_turn = candies_turn + 1 - player_turn
     return bot_turn
 
 
-def p_vs_basic_bot(candies_amount: int, tossup: bool) -> bool:
-    winner = False
+# def p_vs_mind_bot(candies_amount: int, tossup: bool, first_bot_turn: bool) -> bool:
+#     winner = False
 
-    while candies_amount > 0:
-        while tossup:
-            player_turn = player_turn(first_name)
-            winner = False
-            if player_turn < 29:
-                tossup = False
-                print(f'{first_name} take: {player_turn}')
-                candies_amount -= player_turn
-                print(f'Candies left: {candies_amount}')
-            else:
-                print('Incorrect data')
+#     while candies_amount > 0:
+#         while tossup:
+#             winner = False
+#             player_turn = player_turn(first_name)
+#             if player_turn < 29:
+#                 tossup = False
+#                 print(f'{first_name} take: {player_turn}')
+#                 candies_amount -= player_turn
+#                 print(f'Candies left: {candies_amount}')
+#             else:
+#                 print('Incorrect data')
 
-        if (candies_amount > 0 and not tossup):
-            if (candies_amount < 2):
-                winner = True
-                candies_amount -= candies_amount
-            else:
-                bot_player = basic_bot_player()
-                tossup = True
-                print(f'Computer take: {bot_player}')
-                candies_amount -= bot_player
-                print(f'Candies left: {candies_amount}')
-    return winner
-
-
-def p_vs_mind_bot(candies_amount: int, tossup: bool, first_bot_turn: bool) -> bool:
-    winner = False
-
-    while candies_amount > 0:
-        while tossup:
-            winner = False
-            player_turn = player_turn(first_name)
-            if player_turn < 29:
-                tossup = False
-                print(f'{first_name} take: {player_turn}')
-                candies_amount -= player_turn
-                print(f'Candies left: {candies_amount}')
-            else:
-                print('Incorrect data')
-
-        if (candies_amount > 0 and not tossup):
-            if (first_bot_turn):
-                bot_player = 19
-                tossup = True
-                print(f'Computer take: {bot_player}')
-                candies_amount -= bot_player
-                print(f'Candies left: {candies_amount}')
-                first_bot_turn = False
-            elif (candies_amount < 2):
-                winner = True
-                candies_amount -= candies_amount
-            else:
-                bot_player = mind_bot_player(player_turn)
-                tossup = True
-                print(f'Computer take: {bot_player}')
-                candies_amount -= bot_player
-                print(f'Candies left: {candies_amount}')
-    return winner
+#         if (candies_amount > 0 and not tossup):
+#             if (first_bot_turn):
+#                 bot_player = 19 # (amount % cand_turn + 1) - 1
+#                 tossup = True
+#                 print(f'Computer take: {bot_player}')
+#                 candies_amount -= bot_player
+#                 print(f'Candies left: {candies_amount}')
+#                 first_bot_turn = False
+#             elif (candies_amount < 2):
+#                 winner = True
+#                 candies_amount -= candies_amount
+#             else:
+#                 bot_player = mind_bot_player(player_turn)
+#                 tossup = True
+#                 print(f'Computer take: {bot_player}')
+#                 candies_amount -= bot_player
+#                 print(f'Candies left: {candies_amount}')
+#     return winner
 
 
 def help(update, context):
@@ -106,6 +78,26 @@ def cl_start(update, _):
 
 
 def p_vs_p(update, _):
+    global game_choose
+    game_choose = 0
+    update.message.reply_text(
+        "Hello! It's Candies game.\nPlayer 1, what's your name?\nOr /cancel to exit game", 
+        reply_markup=ReplyKeyboardRemove())
+    return FIRST_P
+
+
+def p_vs_easy_bot(update, _):
+    global game_choose
+    game_choose = 1
+    update.message.reply_text(
+        "Hello! It's Candies game.\nPlayer 1, what's your name?\nOr /cancel to exit game", 
+        reply_markup=ReplyKeyboardRemove())
+    return FIRST_P
+
+
+def p_vs_mind_bot(update, _):
+    global game_choose
+    game_choose = 2
     update.message.reply_text(
         "Hello! It's Candies game.\nPlayer 1, what's your name?\nOr /cancel to exit game", 
         reply_markup=ReplyKeyboardRemove())
@@ -113,11 +105,16 @@ def p_vs_p(update, _):
 
 
 def first_p(update, _):
-    global first_name
+    global game_choose, first_name
     first_name = update.message.text
-    update.message.reply_text(
-        f"OK, {update.message.text}.\nAnd player 2, your name is?\nOr /cancel to exit game")
-    return SECOND_P
+    if game_choose == 0:
+        update.message.reply_text(
+            f"OK, {update.message.text}.\nAnd player 2, your name is?\nOr /cancel to exit game")
+        return SECOND_P
+    elif game_choose == 1 or 2:
+        update.message.reply_text(
+            f"OK, {update.message.text}.\nHow many candies put on the table for your game?\nOr /cancel to exit game")
+        return AMOUNT
 
 
 def second_p(update, _):
@@ -131,7 +128,7 @@ def second_p(update, _):
 def amount(update, _):    
     global candies_amount
     candies_amount = None
-    if type(int(update.message.text)) == int:
+    if str(update.message.text).isnumeric():
         candies_amount = int(update.message.text)
         update.message.reply_text(
             f"Good. You have {update.message.text} candies on table.\nHow many candies you want to take for one turn?\nOr /cancel to exit game")
@@ -143,9 +140,9 @@ def amount(update, _):
 
 
 def cand_turn(update, _):
-    global candies_turn, first_name, second_name
+    global game_choose, candies_amount, candies_turn, first_name, second_name
     candies_turn = None
-    if type(int(update.message.text)) == int:
+    if str(update.message.text).isnumeric():
         candies_turn = int(update.message.text)
         turn = first_turn_tossup()
         if turn:
@@ -153,9 +150,43 @@ def cand_turn(update, _):
                 f"Let's go, players. You must take some candies up to {update.message.text} for your turn.\nWho take last candy - lose the game.\nRandom say: '{first_name} turn first'.\nOr /cancel to exit game")
             return EVEN_TURN
         else:
-            update.message.reply_text(
-                f"Let's go, players. You must take some candies up to {update.message.text} for your turn.\nWho take last candy - lose the game.\nRandom say: '{second_name} turn first'.\nOr /cancel to exit game")
-            return ODD_TURN
+            if game_choose == 0:
+                update.message.reply_text(
+                    f"Let's go, players. You must take some candies up to {update.message.text} for your turn.\nWho take last candy - lose the game.\nRandom say: '{second_name} turn first'.\nOr /cancel to exit game")
+                return ODD_TURN
+            elif game_choose == 1:
+                update.message.reply_text(
+                    f"Let's go, players. You must take some candies up to {update.message.text} for your turn.\nWho take last candy - lose the game.\nRandom say: 'Bot turn first'.\nOr /cancel to exit game")
+                bot_player = basic_bot_player()
+                update.message.reply_text(f"{bot_player}")
+                candies_amount -= bot_player
+                if candies_amount < 1:
+                    update.message.reply_text(
+                        f"{first_name} wins the game. Congratulation! Try again /start")
+                    return ConversationHandler.END
+                else:
+                    update.message.reply_text(
+                        f"Bot take: {bot_player}. Candies left: {candies_amount}\n{first_name} your turn")
+                    return EVEN_TURN
+            elif game_choose == 2:
+                update.message.reply_text(
+                    f"Let's go, players. You must take some candies up to {update.message.text} for your turn.\nWho take last candy - lose the game.\nRandom say: 'Bot turn first'.\nOr /cancel to exit game")
+                if candies_amount > 1:
+                    bot_player = (candies_amount % (candies_turn + 1)) - 1
+                    update.message.reply_text(f"{bot_player}")
+                    candies_amount -= bot_player
+                else:
+                    update.message.reply_text(
+                        f"{first_name} wins the game. Congratulation! Try again /start")
+                    return ConversationHandler.END
+                if candies_amount < 1:
+                    update.message.reply_text(
+                        f"{first_name} wins the game. Congratulation! Try again /start")
+                    return ConversationHandler.END
+                else:
+                    update.message.reply_text(
+                        f"Bot take: {bot_player}. Candies left: {candies_amount}\n{first_name} your turn")
+                    return EVEN_TURN
     else:
         update.message.reply_text(
             f"Incorrect data {update.message.text} - not number. Try again /start")
@@ -163,19 +194,57 @@ def cand_turn(update, _):
 
 
 def even_turn(update, _):
-    global candies_amount, candies_turn, first_name, second_name
-    if type(int(update.message.text)) == int:
+    global game_choose, candies_amount, candies_turn, first_name, second_name
+    if str(update.message.text).isnumeric():
         player_turn = int(update.message.text)
         if player_turn < candies_turn + 1:
             candies_amount -= player_turn
             if candies_amount < 1:
-                update.message.reply_text(
-                f"{second_name} wins the game. Congratulation! Try again /start")
+                if game_choose == 0:
+                    update.message.reply_text(
+                        f"{second_name} wins the game. Congratulation! Try again /start")
+                elif game_choose == 1 or 2:
+                    update.message.reply_text(
+                        f"Bot wins the game. Want to revenge? Try again /start")
                 return ConversationHandler.END
             else:
-                update.message.reply_text(
-                f"{first_name} take: {player_turn}. Candies left: {candies_amount}\n{second_name} your turn")
-                return ODD_TURN
+                if game_choose == 0:
+                    update.message.reply_text(
+                        f"{first_name} take: {player_turn}. Candies left: {candies_amount}\n{second_name} your turn")
+                    return ODD_TURN
+                elif game_choose == 1:
+                    update.message.reply_text(
+                        f"{first_name} take: {player_turn}. Candies left: {candies_amount}\nNow, bot turn")
+                    bot_player = basic_bot_player()
+                    update.message.reply_text(f"{bot_player}")
+                    candies_amount -= bot_player
+                    if candies_amount < 1:
+                        update.message.reply_text(
+                            f"{first_name} wins the game. Congratulation! Try again /start")
+                        return ConversationHandler.END
+                    else:
+                        update.message.reply_text(
+                            f"Bot take: {bot_player}. Candies left: {candies_amount}\n{first_name} your turn")
+                        return EVEN_TURN
+                elif game_choose == 2:
+                    update.message.reply_text(
+                        f"{first_name} take: {player_turn}. Candies left: {candies_amount}\nNow, bot turn")
+                    if candies_amount > 1:
+                        bot_player = mind_bot_player(player_turn)
+                        update.message.reply_text(f"{bot_player}")
+                        candies_amount -= bot_player
+                    else:
+                        update.message.reply_text(
+                            f"{first_name} wins the game. Congratulation! Try again /start")
+                        return ConversationHandler.END
+                    if candies_amount < 1:
+                        update.message.reply_text(
+                            f"{first_name} wins the game. Congratulation! Try again /start")
+                        return ConversationHandler.END
+                    else:
+                        update.message.reply_text(
+                            f"Bot take: {bot_player}. Candies left: {candies_amount}\n{first_name} your turn")
+                        return EVEN_TURN
         else:
             update.message.reply_text(
                 f"Incorrect data. Try again /start")
@@ -187,14 +256,14 @@ def even_turn(update, _):
 
 
 def odd_turn(update, _):
-    global candies_amount, candies_turn, second_name
-    if type(int(update.message.text)) == int:
+    global game_choose, candies_amount, candies_turn, second_name
+    if str(update.message.text).isnumeric():
         player_turn = int(update.message.text)
         if player_turn < int(candies_turn) + 1:
             candies_amount -= player_turn
             if candies_amount < 1:
                 update.message.reply_text(
-                f"{first_name} wins the game. Congratulation! Try again /start")
+                    f"{first_name} wins the game. Congratulation! Try again /start")
                 return ConversationHandler.END
             else:
                 update.message.reply_text(
@@ -215,6 +284,8 @@ def cancel(update, _):
     return ConversationHandler.END
 
 
-global first_name, second_name, candies_amount, candies_turn
+global game_choose, first_name, second_name, candies_amount, candies_turn
+
+game_choose = None
 
 FIRST_P, SECOND_P, AMOUNT, CAND_TURN, EVEN_TURN, ODD_TURN = range(6)
--- bot_interface.py
+++ bot_interface.py
@@ -1,4 +1,6 @@
 from bot_comands import *
+
+
 def start_button():
     bot = Bot(token=TOKEN)
     updater = Updater(token=TOKEN)
@@ -8,8 +10,34 @@ def start_button():
     start_handler = CommandHandler('start', start)
     cl_start_handler = CommandHandler('close', cl_start)
 
-    conv_handler_login = ConversationHandler(
+    conv_handler_pvsp = ConversationHandler(
         entry_points=[CommandHandler('p_vs_p', p_vs_p)],
+        states={
+            FIRST_P: [MessageHandler(Filters.text & ~Filters.command, first_p)],
+            SECOND_P: [MessageHandler(Filters.text & ~Filters.command, second_p)],
+            AMOUNT: [MessageHandler(Filters.text & ~Filters.command, amount)],
+            CAND_TURN: [MessageHandler(Filters.text & ~Filters.command, cand_turn)],
+            EVEN_TURN: [MessageHandler(Filters.text & ~Filters.command, even_turn)],
+            ODD_TURN: [MessageHandler(Filters.text & ~Filters.command, odd_turn)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    conv_handler_pvsebot = ConversationHandler(
+        entry_points=[CommandHandler('p_vs_easy_bot', p_vs_easy_bot)],
+        states={
+            FIRST_P: [MessageHandler(Filters.text & ~Filters.command, first_p)],
+            SECOND_P: [MessageHandler(Filters.text & ~Filters.command, second_p)],
+            AMOUNT: [MessageHandler(Filters.text & ~Filters.command, amount)],
+            CAND_TURN: [MessageHandler(Filters.text & ~Filters.command, cand_turn)],
+            EVEN_TURN: [MessageHandler(Filters.text & ~Filters.command, even_turn)],
+            ODD_TURN: [MessageHandler(Filters.text & ~Filters.command, odd_turn)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    conv_handler_pvsmbot = ConversationHandler(
+        entry_points=[CommandHandler('p_vs_mind_bot', p_vs_mind_bot)],
         states={
             FIRST_P: [MessageHandler(Filters.text & ~Filters.command, first_p)],
             SECOND_P: [MessageHandler(Filters.text & ~Filters.command, second_p)],
@@ -28,12 +56,12 @@ def start_button():
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(cl_start_handler)
 
-    dispatcher.add_handler(conv_handler_login)
-
+    dispatcher.add_handler(conv_handler_pvsp)
+    dispatcher.add_handler(conv_handler_pvsebot)
+    dispatcher.add_handler(conv_handler_pvsmbot)
 
     # dispatcher.add_handler(message_handler)
     dispatcher.add_handler(unknown_handler)
-
 
     print('server started')
     updater.start_polling()
